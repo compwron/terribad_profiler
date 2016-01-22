0a6fd2334bc9c53fa929992e82cfbbfc63992a3f
compwron <compiledwrong@gmail.com>
2016-01-21 20:36:51 -0800

    tests

--- spec/profiler_spec.rb
+++ spec/profiler_spec.rb
@@ -3,5 +3,12 @@
 describe Profiler do
   subject { described_class.new(example_1).profile! }
   it "records line count" do
+    time = Time.new(2016, 1, 21)
+    Timecop.freeze(time) do
+      expect(subject).to eq [{line_number: 1,
+        time_before_line: [time],
+        time_after_line: [time],
+        execution_count: 1}]
+    end
   end
 end
