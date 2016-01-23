3e9449de982f8a9892aca398580474c9edbb691f
compwron <compiledwrong@gmail.com>
2016-01-22 20:54:51 -0800

    data for lines

--- lib/profiler.rb
+++ lib/profiler.rb
@@ -1,31 +1,59 @@
 class Profiler
   def initialize(filepath)
     @original_filename = File.basename(filepath)
-    @original_file = File.open(filepath)
+    @original_file_contents = File.read(filepath)
   end
 
   def profile!
-    # binding.pry
     new_filepath = "tmp/annotated_#{@original_filename}"
-    f = File.open(new_filepath, "w")
-    f.puts annotate(@original_file)
-    annotation_output = `ruby new_filepath`
-    @annotation_data = parse(annotation_output)
-    [{line_number: 1,
-           time_before_line: [Time.now],
-           time_after_line: [Time.now],
-           execution_count: 1}]
+    code_with_annotations = annotate(@original_file_contents)
+    File.open(new_filepath, "w") {|f|
+      f.write code_with_annotations.join("\n")
+    }
+    a = File.read(new_filepath)
+    annotation_output = `ruby #{new_filepath}`
+    @annotation_data = parse(annotation_output.split("\n"))
   end
 
   private
 
-  def annotate(file)
-    contents = file.read
-    binding.pry
-    p contents
+  def annotate(file_contents)
+    contents = file_contents.split("\n")
+    annotated_contents = []
+    contents.each_with_index {|line, line_number|
+      annotated_contents << anno(line, line_number)
+    }
+    annotated_contents
+  end
+
+  def anno(line, line_number)
+    [
+      "puts \"line_number:#{line_number},BEFORE,#\{Time.now.utc\}\"",
+      "#{line}",
+      "puts \"line_number:#{line_number},AFTER,#\{Time.now.utc\}\""
+    ].join("\n")
   end
 
   def parse(output)
+    annotation_data = {}
+    # "line_number:2,AFTER,#{Time.now}"
+    # binding.pry
+    output.each {|line|
+      if line.include?("BEFORE") || line.include?("AFTER")
+        line_number = line.split(",")[0].split(":")[1].to_i
+        annotation_data[line_number] ||= {time_before_line: [], time_after_line: [], execution_count: 0}
+        timesstamp = line.split(",")[2]
+        if line.include?("BEFORE")
+          annotation_data[line_number][:time_before_line] << Time.parse(timesstamp)
+        elsif line.include?("AFTER")
+          annotation_data[line_number][:time_after_line] << Time.parse(timesstamp)
+        end
+      end
+    }
+    annotation_data.each {|k, v|
+      v[:execution_count] = [v[:time_before_line].count, v[:time_after_line].count].min
+    }
+    annotation_data
   end
 end
 
--- spec/profiler_spec.rb
+++ spec/profiler_spec.rb
@@ -1,38 +1,33 @@
 describe Profiler do
   subject { described_class.new(example).profile! }
-  let(:time) { Time.new(2016, 1, 21) }
+  let(:time) { Time.utc(2016, 1, 21) }
 
   context "with one line example" do
     let(:example) { "samples/1.rb" }
     it "records line count" do
-      Timecop.freeze(time) do
-        expect(subject).to eq [{line_number: 1,
-          time_before_line: [time],
-          time_after_line: [time],
-          execution_count: 1}]
-      end
+      expect(subject.size).to eq 1
+      expect(subject[0][:execution_count]).to eq 1.0
+      expect(subject[0][:time_after_line].size).to be 1
+      expect(subject[0][:time_before_line].size).to be 1
+      expect(subject[0][:time_before_line].first).to be <= Time.now.utc
     end
   end
 
   context "with loop example" do
     let(:example) { "samples/2.rb" }
     it "records line count" do
-      Timecop.freeze(time) do
-        expect(subject).to eq [
-          { line_number: 1,
-            time_before_line: [time],
-            time_after_line: [time],
-            execution_count: 1},
-          { line_number: 2,
-            time_before_line: [time, time],
-            time_after_line: [time, time],
-            execution_count: 3},
-          { line_number: 3,
-            time_before_line: [time],
-            time_after_line: [time],
-            execution_count: 1},
-          ]
-      end
+      expect(subject.size).to eq 3
+      expect(subject[0][:execution_count]).to eq 1
+      expect(subject[0][:time_before_line].size).to be 1
+      expect(subject[0][:time_after_line].size).to be 3
+
+      expect(subject[1][:execution_count]).to eq 3.0
+      expect(subject[1][:time_before_line].size).to be 3
+      expect(subject[1][:time_after_line].size).to be 3
+
+      expect(subject[2][:execution_count]).to eq 1
+      expect(subject[2][:time_before_line].size).to be 3
+      expect(subject[2][:time_after_line].size).to be 1
     end
   end
 end
