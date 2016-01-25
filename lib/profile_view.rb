a79db7d938a44bdacbac3d9723c7014e47d7f83c
compwron <compiledwrong@gmail.com>
2016-01-24 16:21:53 -0800

    fixing test, removing print

--- lib/profile_view.rb
+++ lib/profile_view.rb
@@ -83,8 +83,6 @@ class ProfileView
     end
 
     results_filename = "tmp/html/overview_#{@original_filename}.html"
-    p results_filename
-    p @original_file_contents
     File.open(results_filename, "w") do |f|
       f.write(mab.to_s)
     end
