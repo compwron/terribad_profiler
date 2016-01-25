8097862dcdda25b7b455efde5295ea4e75c9ae22
compwron <compiledwrong@gmail.com>
2016-01-24 19:38:13 -0800

    refactor out the parser

---
+++ lib/parser.rb
@@ -0,0 +1,35 @@
+class Parser
+  attr_reader :parsed_data
+  BEFORE = "BEFORE"
+  AFTER = "AFTER"
+
+  def initialize(output)
+    @parsed_data = parse(output)
+  end
+
+  private
+
+  def parse(output)
+    annotation_data = {}
+    output.each {|line|
+      if is_annotation_output?(line)
+        line_number = line.split(",")[0].split(":")[1].to_i
+        annotation_data[line_number] ||= {time_before_line: [], time_after_line: [], execution_count: 0}
+        timesstamp = line.split(",")[2]
+        if line.include?(BEFORE)
+          annotation_data[line_number][:time_before_line] << Time.parse(timesstamp)
+        elsif line.include?(AFTER)
+          annotation_data[line_number][:time_after_line] << Time.parse(timesstamp)
+        end
+      end
+    }
+    annotation_data.each {|k, v|
+      v[:execution_count] = [v[:time_before_line].count, v[:time_after_line].count].min
+    }
+    annotation_data
+  end
+
+  def is_annotation_output?(line)
+    line.include?(BEFORE) || line.include?(AFTER)
+  end
+end
--- lib/profiler.rb
+++ lib/profiler.rb
@@ -1,4 +1,7 @@
 require 'markaby'
+require_relative "parser"
+require_relative "profile_view"
+
 class Profiler
   def initialize(filepath)
     @original_filename = File.basename(filepath)
@@ -10,8 +13,19 @@ class Profiler
       f.write annotate(@original_file_contents).join("\n")
     }
     annotation_output = `ruby #{new_filepath}`.split("\n")
-    @annotation_data = parse(annotation_output)
+    @annotation_data = Parser.new(annotation_output).parsed_data
   end
+
+  def overview
+    profile!
+    html = ProfileView.new(@original_file_contents, @annotation_data, @original_filename).overview
+    File.open(results_filename, "w") do |f|
+      f.write(html)
+    end
+    html
+  end
+
+  private
 
   def info(line_number)
     data = @annotation_data[line_number]
@@ -26,17 +40,6 @@ class Profiler
     avg_execution_time = total_execution_time / data[:execution_count]
     {execution_count: data[:execution_count], avg_execution_time: avg_execution_time, total_execution_time: total_execution_time}
   end
-
-  def overview
-    profile!
-    html = ProfileView.new(@original_file_contents, @annotation_data, @original_filename).overview
-    File.open(results_filename, "w") do |f|
-      f.write(html)
-    end
-    html
-  end
-
-  private
 
   def results_filename
     "tmp/html/overview_#{@original_filename}.html"
@@ -72,28 +75,6 @@ class Profiler
       "#{line}",
       "puts \"line_number:#{line_number},AFTER,#\{Time.now.utc\}\""
     ].join("\n")
-  end
-
-  def parse(output)
-    annotation_data = {}
-    # "line_number:2,AFTER,#{Time.now}"
-    # binding.pry
-    output.each {|line|
-      if line.include?("BEFORE") || line.include?("AFTER")
-        line_number = line.split(",")[0].split(":")[1].to_i
-        annotation_data[line_number] ||= {time_before_line: [], time_after_line: [], execution_count: 0}
-        timesstamp = line.split(",")[2]
-        if line.include?("BEFORE")
-          annotation_data[line_number][:time_before_line] << Time.parse(timesstamp)
-        elsif line.include?("AFTER")
-          annotation_data[line_number][:time_after_line] << Time.parse(timesstamp)
-        end
-      end
-    }
-    annotation_data.each {|k, v|
-      v[:execution_count] = [v[:time_before_line].count, v[:time_after_line].count].min
-    }
-    annotation_data
   end
 end
 
